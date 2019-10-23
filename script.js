9eed564eca393ef6504598d7f1240ffe8270d208
xMaxxxz <maxdenboef@hotmail.com>
2019-10-24 00:49:31 +0200

    Add button to choose grid size. Add different color options.

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-//Grid stuff
+//UI
+let colorOptions = document.querySelectorAll(".color");
+let color;
+colorOptions.forEach(function(e) {
+    e.addEventListener("click", function(e) {
+        color = e.target.getAttribute("id");
+    });
+});
+
+//Creating grid
 
 let gridArray = [];
 
 function createGrid(size) {
     let grid = document.querySelector("#grid");
     let elementWidth = (grid.clientWidth / Math.sqrt(size)) / 2 - 1.1; //Substracting 1.1 because of the borders + precision error
+    
+    let firstElement = grid.firstElementChild;
+    while (firstElement) {
+        firstElement.remove();
+        firstElement = grid.firstElementChild;
+    }
 
     for (let i = 0; i < size; i++) {
         gridArray[i] = document.createElement("div");
@@ -12,12 +27,26 @@ function createGrid(size) {
         gridArray[i].classList.add("grid-element");
         grid.appendChild(gridArray[i]);
     }
+    colorGrid();
 }
+//Default grid size and color
 createGrid(64);
+colorGrid();
 
-let gridElements = document.querySelectorAll(".grid-element");
-gridElements.forEach(function(e) {
-    e.addEventListener("mouseenter", function(e) {
-        e.target.classList.add("black");
-    });
+//Create a new grid
+let btn = document.querySelector("#grid-btn");
+btn.addEventListener("click", function() {
+let size = prompt("What area grid would you like?");
+createGrid(size);
 });
+
+//Coloring grid
+function colorGrid() {
+    let gridElements = document.querySelectorAll(".grid-element");
+    gridElements.forEach(function(e) {
+        e.addEventListener("mouseenter", function(e) {
+            e.target.removeAttribute("class");
+            e.target.classList.add("grid-element", color);
+        });
+    });
+}
